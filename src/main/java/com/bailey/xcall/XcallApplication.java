9907456377f24f893333b4ad9b082c15a111b9a1
priest-2105 <fawzybailey782@gmail.com>
2024-08-08 15:39:15 +0100

    reinit ✨✨

---
+++ src/main/java/com/bailey/xcall/XcallApplication.java
@@ -0,0 +1,13 @@
+package com.bailey.xcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bailey/xcall/XcallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bailey.xcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
